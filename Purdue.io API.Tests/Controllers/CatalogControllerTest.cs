9365db1860e0bc5442951438e3221eafdc8beb4e
Darren Cheng <zades@outlook.com>
2014-10-08 00:10:45 -0400

    Implemented GET: Catalog/Course (ex. Catalog/MA261) and its test method and added skeletons for GET: Catalog/Course/ClassGUID and GET: Catalog/Course/ClassGUID/SectionGUID

--- Purdue.io API.Tests/Controllers/CatalogControllerTest.cs
+++ Purdue.io API.Tests/Controllers/CatalogControllerTest.cs
@@ -21,12 +21,71 @@ namespace PurdueIo.Tests.Controllers
 		public void TestFetchAllCourses()
 		{
 			GenerateTestSchool();
+
 			var controller = new CatalogController();
 			var response = controller.Get() as OkNegotiatedContentResult<IEnumerable<CourseViewModel>>;
 			var courses = response.Content.ToList();
 			
 			Assert.IsTrue(courses.Count > 0, "Course request returned no courses.");
 			Assert.IsTrue(courses.Where(c => c.Title.Equals("Software Engineering")).Count() > 0, "Software Engineering course wasn't returned.");
+		}
+
+		[TestMethod]
+		public void TestFetchSpecifiedCourses()
+		{
+			GenerateTestSchool();
+
+			//Using vars cause it looks prettier :D
+			//Correct input: CS30700
+			var controller = new CatalogController();
+			var response = controller.Get("CS30700") as OkNegotiatedContentResult<IEnumerable<CourseViewModel>>;
+			var courses = response.Content.ToList();
+
+			Assert.IsTrue(courses.Count > 0, "Course request returned no courses, CS30700 should exist in the catalog.");
+			Assert.IsTrue(courses.Where(x => x.Subject.Abbreviation == "CS" && x.Number == "30700").Count() > 0, "CS30700 courses wasn't returned.");
+
+			//Correct input but shortened: CS307
+			response = controller.Get("CS307") as OkNegotiatedContentResult<IEnumerable<CourseViewModel>>;
+			courses = response.Content.ToList();
+
+			Assert.IsTrue(courses.Count > 0, "Course request returned no courses, CS307 should exist in the catalog.");
+			Assert.IsTrue(courses.Where(x => x.Subject.Abbreviation == "CS" && x.Number == "30700").Count() > 0, "CS307 courses wasn't returned.");
+
+			//Invalid class: POOP10100
+			response = controller.Get("POOP10100") as OkNegotiatedContentResult<IEnumerable<CourseViewModel>>;
+			courses = response.Content.ToList();
+
+			Assert.IsTrue(courses.Count == 0, "Course request should return no classes, POOP10100 should not be a valid course.");
+
+			//Invalid class but shortened: POOP101
+			response = controller.Get("POOP101") as OkNegotiatedContentResult<IEnumerable<CourseViewModel>>;
+			courses = response.Content.ToList();
+
+			Assert.IsTrue(courses.Count == 0, "Course request should return no classes, POOP101 should not be a valid course.");
+
+			//Format check: CS30777
+			var badResponse = controller.Get("CS30777");
+			Assert.IsInstanceOfType(badResponse, typeof(BadRequestErrorMessageResult), "CS30777 is not an valid course");
+
+			//Format check: CS 30700
+			badResponse = controller.Get("CS 30700");
+			Assert.IsInstanceOfType(badResponse, typeof(BadRequestErrorMessageResult), "CS 30700 is not an valid course");
+
+			//Format check: CS1
+			badResponse = controller.Get("CS1");
+			Assert.IsInstanceOfType(badResponse, typeof(BadRequestErrorMessageResult), "CS1 is not an valid course");
+
+			//Format check: CS1111111
+			badResponse = controller.Get("CS1111111");
+			Assert.IsInstanceOfType(badResponse, typeof(BadRequestErrorMessageResult), "CS1111111 is not an valid course");
+
+			//Format check: CS
+			badResponse = controller.Get("CS");
+			Assert.IsInstanceOfType(badResponse, typeof(BadRequestErrorMessageResult), "CS is not an valid course");
+
+			//Format check: 111
+			badResponse = controller.Get("111");
+			Assert.IsInstanceOfType(badResponse, typeof(BadRequestErrorMessageResult), "111 is not an valid course");
 		}
 	}
 }
--- Purdue.io API/Controllers/CatalogController.cs
+++ Purdue.io API/Controllers/CatalogController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Web.Http;
 using System.Web.Http.Description;
 using System.Web.Http.Results;
@@ -14,6 +15,13 @@ namespace PurdueIo.Controllers
 	[RoutePrefix("Catalog")]
     public class CatalogController : ApiController
     {
+		//Regex capture group names
+		private const string COURSE_SUBJECT_CAPTURE_GROUP_NAME	= "subject";
+		private const string COURSE_NUMBER_CAPTURE_GROUP_NAME	= "number";
+
+		//Regex strings
+		private const string COURSE_SUBJECT_NUMBER_REGEX		= @"\A(?<" + COURSE_SUBJECT_CAPTURE_GROUP_NAME + @">[A-Za-z]+)(?<" + COURSE_NUMBER_CAPTURE_GROUP_NAME + @">\d{3}(?:00)?)\z";
+
 		// This DB really should be instantiated on its own in each method... but that causes disposed problems.
 		private ApplicationDbContext _Db = new ApplicationDbContext(); 
 
@@ -23,7 +31,8 @@ namespace PurdueIo.Controllers
 		public IHttpActionResult Get()
         {
 			// Get all of the courses, convert to viewmodel.
-			var allcourses = _Db.Courses.ToList().Select(x => x.ToViewModel());
+			IEnumerable<CourseViewModel> allcourses = _Db.Courses.ToList().Select(x => x.ToViewModel());
+
 			// Return w/ Ok status code.
 			return Ok<IEnumerable<CourseViewModel>>(allcourses);
         }
@@ -31,10 +40,50 @@ namespace PurdueIo.Controllers
         // GET: Catalog/MA261
 		[Route("{course}")]
 		[ResponseType(typeof (IEnumerable<CourseViewModel>))]
-		public IHttpActionResult Get(string course)
+		public IHttpActionResult Get(String course)
         {
-			// Not implemented.
-			return Ok();
+			//Init regex
+			Regex regex = new Regex(COURSE_SUBJECT_NUMBER_REGEX);
+			Match match = regex.Match(course);
+			
+			//If there are no matches, exit with error
+			if(!match.Success)
+			{
+				//error, invalid format
+				return BadRequest("Invalid course format.  Course should be subject abbrivation followed by the course number (ex. PHYS172 or PHYS172000)");
+			}
+
+			//Capture subject and number group from the string
+			String courseSubject = match.Groups[COURSE_SUBJECT_CAPTURE_GROUP_NAME].Value;
+			String courseNumber = match.Groups[COURSE_NUMBER_CAPTURE_GROUP_NAME].Value;
+			
+			//Add zeros to number if number is only 3 characters (ex. 390 -> 39000)
+			if(courseNumber.Length == 3)
+			{
+				courseNumber += "00";
+			}
+
+			IEnumerable<CourseViewModel> selectedCourses = _Db.Courses.Where(x => x.Number == courseNumber && x.Subject.Abbreviation == courseSubject).ToList().Select(x => x.ToViewModel());
+
+			return Ok<IEnumerable<CourseViewModel>>(selectedCourses);
         }
+
+		//GET: Catalog/MA261/[ClassGUID]
+		[Route("{course}/{classGUID}")]
+		[ResponseType(typeof(IEnumerable<ClassViewModel>))]
+		public IHttpActionResult Get(String course, String classGUID)
+		{
+			//TODO: Implenment
+			return Ok();
+		}
+
+		//GET: Catalog/MA261/[ClassGUID]/[SectionGUID]
+		[Route("{course}/{classGUID}/{sectionGUID}")]
+		[ResponseType(typeof(IEnumerable<ClassViewModel>))]
+		public IHttpActionResult Get(String course, String classGUID, String sectionGUID)
+		{
+			//TODO: Implenment
+			return Ok();
+		}
     }
 }
