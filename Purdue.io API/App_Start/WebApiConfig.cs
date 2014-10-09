e9a493870507f95221aa375a580614addada1644
Darren Cheng <zades@outlook.com>
2014-10-09 05:28:59 -0400

    Added Odata libraries, Added some code for Odata, added code for getting All[object], /odata/$metadata now work

--- Purdue.io API/App_Start/WebApiConfig.cs
+++ Purdue.io API/App_Start/WebApiConfig.cs
@@ -5,6 +5,9 @@ using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using System.Web.OData.Builder;
+using System.Web.OData.Extensions;
+using PurdueIo.Models.Catalog;
 
 namespace PurdueIo
 {
@@ -28,6 +31,38 @@ namespace PurdueIo
 				routeTemplate: "api/{controller}/{id}",
 				defaults: new { id = RouteParameter.Optional }
 			);
+			config.MapODataServiceRoute("odata", "odata", model: GetModel());
+
+		}
+		public static Microsoft.OData.Edm.IEdmModel GetModel()
+		{
+			//OData
+			ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+			//EntityTypeConfiguration<Term> termType = builder.EntityType<Term>();
+			//EntityTypeConfiguration<Section> sectionType = builder.EntityType<Section>();
+
+			//termType.Ignore(c => c.StartDate);
+			//termType.Ignore(c => c.EndDate);
+			//sectionType.Ignore(c => c.StartDate);
+			//sectionType.Ignore(c => c.EndDate);
+
+			builder.EntitySet<Course>("Courses");
+			builder.EntitySet<Class>("Classes");
+			builder.EntitySet<Section>("Sections");
+			builder.EntitySet<Term>("Terms");
+			builder.EntitySet<Campus>("Campuses");
+			builder.EntitySet<Building>("Buildings");
+			builder.EntitySet<Room>("Rooms");
+			builder.EntitySet<Instructor>("Instructors");
+
+
+			//EntitySetConfiguration<Course> courses = builder.EntitySet<Course>("Course");
+
+			//FunctionConfiguration myFirstFunction = courses.EntityType.Collection.Function("MyFirstFunction");
+			//myFirstFunction.ReturnsCollectionFromEntitySet<Course>("Course");
+
+
+			return builder.GetEdmModel();
 		}
 	}
 }
