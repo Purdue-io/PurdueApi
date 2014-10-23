31165dc1529927ce50f1682178dff33fe81a93ef
Darren Cheng <zades@outlook.com>
2014-10-22 22:42:05 -0400

    Added the rest of odata endpoints

---
+++ Purdue.io API/Controllers/Odata/BuildingsController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using System.Web.OData;
+using System.Web.OData.Routing;
+using PurdueIoDb;
+using PurdueIoDb.Catalog;
+
+namespace PurdueIo.Controllers.Odata
+{
+	[ODataRoutePrefix("Buildings")]
+	public class BuildingsController : ODataController
+	{
+		private ApplicationDbContext db = new ApplicationDbContext();
+
+		// GET: odata/Buildings
+		/// <summary>
+		/// Returns all buildings in existance
+		/// </summary>
+		/// <returns></returns>
+		[HttpGet]
+		[ODataRoute]
+		[EnableQuery(MaxAnyAllExpressionDepth = 2)]
+		public IHttpActionResult GetBuildings()
+		{
+			return Ok(db.Buildings);
+		}
+
+		// GET: odata/Buildings(5)
+		/// <summary>
+		/// Returns a building given the guid
+		/// </summary>
+		/// <param name="buildingKey"></param>
+		/// <returns></returns>
+		[HttpGet]
+		[ODataRoute("({buildingKey})")]
+		[EnableQuery(MaxAnyAllExpressionDepth = 2)]
+		public IHttpActionResult GetBuilding([FromODataUri] Guid buildingKey)
+		{
+			return Ok(SingleResult.Create(db.Buildings.Where(building => building.BuildingId == buildingKey)));
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				db.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		private bool BuildingExists(Guid key)
+		{
+			return db.Buildings.Count(e => e.BuildingId == key) > 0;
+		}
+	}
+}
---
+++ Purdue.io API/Controllers/Odata/SubjectsController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using System.Web.OData;
+using System.Web.OData.Routing;
+using PurdueIoDb;
+using PurdueIoDb.Catalog;
+
+namespace PurdueIo.Controllers.Odata
+{
+	[ODataRoutePrefix("Subjects")]
+	public class SubjectController : ODataController
+	{
+		private ApplicationDbContext db = new ApplicationDbContext();
+
+		// GET: odata/Subject
+		/// <summary>
+		/// Returns all subjects in existance
+		/// </summary>
+		/// <returns></returns>
+		[HttpGet]
+		[ODataRoute]
+		[EnableQuery(MaxAnyAllExpressionDepth = 2)]
+		public IHttpActionResult GetSubjects()
+		{
+			return Ok(db.Subjects);
+		}
+
+		// GET: odata/Subject(5)
+		/// <summary>
+		/// Returns a subject given the guid
+		/// </summary>
+		/// <param name="subjectKey"></param>
+		/// <returns></returns>
+		[HttpGet]
+		[ODataRoute("({subjectKey})")]
+		[EnableQuery(MaxAnyAllExpressionDepth = 2)]
+		public IHttpActionResult GetSubject([FromODataUri] Guid subjectKey)
+		{
+			return Ok(SingleResult.Create(db.Subjects.Where(subject => subject.SubjectId == subjectKey)));
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				db.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		private bool SubjectExists(Guid key)
+		{
+			return db.Subjects.Count(e => e.SubjectId == key) > 0;
+		}
+	}
+}
